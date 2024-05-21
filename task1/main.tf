b38ef342ce6cba6b32ce262af2f580b7b5193fd5
Csaba Szilagyi <ilyennevnincsaz100@gmail.com>
2024-05-21 10:00:16 +0200

    make vars configurable from outside of module

--- task1/main.tf
+++ task1/main.tf
@@ -1,3 +1,10 @@
 module "vpc" {
-    source = "./modules/vpc/"
+    source               = "./modules/vpc/"
+
+    region               = var.region
+    vpc_cidr_block       = var.vpc_cidr_block
+    vpc_name             = var.vpc_name
+    public_subnet_cidrs  = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    azs                  = var.azs
 }
--- task1/variables.tf
+++ task1/variables.tf
@@ -3,3 +3,33 @@ variable "region" {
   default     = "eu-central-1"
   description = "AWS Region"
 }
+
+variable vpc_cidr_block {
+  type        = string
+  default     = "10.0.0.0/16"
+  description = "VPC CIDR block"
+}
+
+variable vpc_name {
+  type        = string
+  default     = "hw_vpc"
+  description = "description"
+}
+
+variable "public_subnet_cidrs" {
+ type        = list(string)
+ description = "Public Subnet CIDR values"
+ default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+ 
+variable "private_subnet_cidrs" {
+ type        = list(string)
+ description = "Private Subnet CIDR values"
+ default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "azs" {
+ type        = list(string)
+ description = "Availability Zones"
+ default     = ["eu-central-1a", "eu-central-1b"]
+}
